6a30598e6a8f7626f664c99a7bef41343cef96cd
Victor <00001225470961sp@al.educacao.sp.gov.br>
2026-08-14 15:53:15 -0300

    atualização de trailers
    botei os trailers para cada coisa

--- filmes.js
+++ filmes.js
@@ -2,66 +2,90 @@
 
 export const filmes = [
     {
+        id: "interestelar",
         titulo: "Interestelar",
         imagem: "Imagens/Interstellar_Filme.png",
-        descricao: "Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo lar para a humanidade."
+        descricao: "Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo lar para a humanidade.",
+        trailer: "i6avfCqKcQo"
     },
     {
+        id: "batman",
         titulo: "Batman",
         imagem: "Imagens/batman.jpg",
-        descricao: "O herói de Gotham enfrenta novos desafios."
+        descricao: "O herói de Gotham enfrenta novos desafios.",
+        trailer: "zqfz04yCTnE"
     },
     {
+        id: "godzilla-2-rei-dos-monstros",
         titulo: "Godzilla 2: Rei dos Monstros",
         imagem: "Imagens/godzilla2.jpg",
-        descricao: "O rei encontra uma nova ameaça em seu reino."
+        descricao: "O rei encontra uma nova ameaça em seu reino.",
+        trailer: "Cve9ouQp0tE"
     },
     {
+        id: "shin-godzilla",
         titulo: "Shin Godzilla",
         imagem: "Imagens/shingodzilla.jpg",
-        descricao: "A humanidade se depara com a encarnação da destruição."
+        descricao: "A humanidade se depara com a encarnação da destruição.",
+        trailer: "eNox1a18qqw"
     },
     {
+        id: "michael",
         titulo: "Michael",
         imagem: "Imagens/michael.jpg",
-        descricao: "A história do grande rei do pop Michael Jackson."
+        descricao: "A história do grande rei do pop Michael Jackson.",
+        trailer: "14YXeHKOBUY"
     },
     {
+        id: "homem-aranha-um-novo-dia",
         titulo: "Homem-Aranha: Um Novo Dia",
         imagem: "Imagens/homemaranha.jpg",
-        descricao: "O grande herói enfrenta novas ameaças e encara as mudanças na sua vida."
+        descricao: "O grande herói enfrenta novas ameaças e encara as mudanças na sua vida.",
+        trailer: "ZHMY6nvZAqs"
     }
 ];
 
 export const series = [
     {
+        id: "stranger-things",
         titulo: "Stranger Things",
         imagem: "Imagens/Strangerthings_serie.jpg",
-        descricao: "Um grupo de crianças enfrenta forças sobrenaturais."
+        descricao: "Um grupo de crianças enfrenta forças sobrenaturais.",
+        trailer: "Wb3kaP580kA"
     },
     {
+        id: "dark",
         titulo: "Dark",
         imagem: "Imagens/dark.jpg",
-        descricao: "Mistérios envolvendo viagens no tempo."
+        descricao: "Mistérios envolvendo viagens no tempo.",
+        trailer: "JCCssUOtn2E"
     },
     {
+        id: "cobra-kai",
         titulo: "Cobra Kai",
         imagem: "Imagens/cobrakai.jpg",
-        descricao: "Um ex-campão degenerado de karatê decide começar sua redenção em vida e ensina karatê para uma nova geração."
+        descricao: "Um ex-campão degenerado de karatê decide começar sua redenção em vida e ensina karatê para uma nova geração.",
+        trailer: "dRtrKQa_mms"
     },
     {
+        id: "breaking-bad",
         titulo: "Breaking Bad",
         imagem: "Imagens/breakingbad.jpg",
-        descricao: "Um professor de química com câncer pulmonar decide viver do contrabando de drogas para ganhar dinheiro."
+        descricao: "Um professor de química com câncer pulmonar decide viver do contrabando de drogas para ganhar dinheiro.",
+        trailer: "2gTC4uWP3_Y"
     },
     {
+        id: "bleach",
         titulo: "Bleach",
         imagem: "Imagens/bleach.jpg",
-        descricao: "Um adolescente que consegue ver espiritos se torna um ceifador de almas e luta contra vários inimigos que querem destruir o mundo."
+        descricao: "Um adolescente que consegue ver espiritos se torna um ceifador de almas e luta contra vários inimigos que querem destruir o mundo.",
+        trailer: "ASobpXYYoVQ"
     },
     {
+        id: "neon-genesis-evangelion",
         titulo: "Neon Genesis Evangelion",
         imagem: "Imagens/nge.jpg",
-        descricao: "Shinji Ikari decide ir visitar seu pai e luta contra anjos para impedir o Terceiro Impacto."
+        descricao: "Shinji Ikari decide ir visitar seu pai e luta contra anjos para impedir o Terceiro Impacto.",
+        trailer: "13nSISwxrY4"
     }
 ];
--- script.js
+++ script.js
@@ -1,84 +1,89 @@
 import { filmes, series } from "./filmes.js";    
-    
+
     // =========================
     // FUNÇÃO CRIAR CARD
     // =========================
-    
-    function criarCard(item){
-    
+
+    function criarCard(item, tipo){
+
         return `
             <div class="card">
-    
+
                 <img src="${item.imagem}" alt="${item.titulo}">
-    
+
                 <h3>${item.titulo}</h3>
-    
+
                 <p>${item.descricao}</p>
-    
+
                 <div class="botoes">
-    
-                    <button onclick="assistir('${item.titulo}')">
+
+                    <a
+                        href="player.html?tipo=${tipo}&id=${item.id}"
+                        class="btnPlay"
+                    >
                         ▶ Assistir
-                    </button>
-    
+                    </a>
+
                     <button onclick="favoritar('${item.titulo}')">
                         + Minha Lista
                     </button>
-    
+
                 </div>
-    
+
             </div>
         `;
     }
-    
-    
+
+
     // =========================
     // MOSTRAR FILMES E SÉRIES
     // =========================
-    
+
     function mostrar(){
-    
+
         let listaFilmes = document.getElementById("listaFilmes");
         let listaSeries = document.getElementById("listaSeries");
-    
-        
+
+
         listaFilmes.innerHTML = "";
         listaSeries.innerHTML = "";
-    
-    
+
+
         filmes.forEach(function(f){
-            listaFilmes.innerHTML += criarCard(f);
+            listaFilmes.innerHTML += criarCard(f, "filme");
         });
-    
+
         series.forEach(function(s){
-            listaSeries.innerHTML += criarCard(s);
+            listaSeries.innerHTML += criarCard(s, "serie");
         });
-    
+
     }
-    
-    
+
+
     // =========================
     // BOTÃO ASSISTIR
     // =========================
-    
+
     function assistir(nome){
-    
+
         alert("▶ Reproduzindo: " + nome);
-    
+
     }
-    
-    
+
+
     // =========================
     // FAVORITOS
     // =========================
-    
+
     function favoritar(nome){
-    
+
         alert("⭐ Adicionado à sua lista: " + nome);
-    
+
     }
-    
-    
+
+    window.favoritar = favoritar;
+
+
     // INICIAR
-    
+
     mostrar();
